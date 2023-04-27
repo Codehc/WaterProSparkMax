7df94a0d28b6a99e7715cb6707669ba22b3abde1
Reza Bagheri <rezq.bagheri@gmail.com>
2023-04-27 08:48:46 -0700

    Initial Commit

---
+++ WaterProSparkMax/WaterProSparkMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterProSparkMaxApp.swift
+//  WaterProSparkMax
+//
+//  Created by Reza Bagheri on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WaterProSparkMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
