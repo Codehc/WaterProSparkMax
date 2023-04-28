3dae87f6df3b171da04df7657b03e916a9aba475
Reza Bagheri <rezq.bagheri@gmail.com>
2023-04-27 20:33:21 -0700

    Got some of the basics down. Making progress towards mimicking the ios timer's looks

---
+++ WaterProSparkMax/Config.swift
@@ -0,0 +1,17 @@
+//
+//  Data.swift
+//  WaterProSparkMax
+//
+//  Created by Reza Bagheri on 4/27/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Config {
+    var table: String
+    var usingPurelyInterval: Bool
+    var interval: Int
+    var wateringThreshold: Int
+    var wateringTime: Int
+}
--- WaterProSparkMax/ContentView.swift
+++ WaterProSparkMax/ContentView.swift
@@ -8,15 +8,151 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var config = Config(
+        table: "", usingPurelyInterval: false, interval: 2000, wateringThreshold: 20, wateringTime: 5000
+    )
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("WaterProSparkMax!")
+            TextField("Plant Name", text: $config.table)
+            Toggle("Disable Smart Watering", isOn: $config.usingPurelyInterval)
+            TimePicker()
         }
         .padding()
     }
+}
+
+struct TimePicker: View {
+    @State private var days = 0;
+    @State private var hours = 0;
+    @State private var minutes = 0;
+    @State private var seconds = 0;
+    
+    var body: some View {
+        UnitTimePicker()
+        .padding()
+    }
+}
+
+struct UnitTimePicker: View {
+    @State private var scrollOffset: CGFloat = 0
+    @State private var isScrolling = false
+
+    var body: some View {
+        HStack {
+            ScrollView(showsIndicators: false) {
+                GeometryReader { geometry in
+                    Color.clear.preference(
+                        key: ScrollOffsetKey.self,
+                        value: geometry.frame(in: .named("scrollView")).origin.y
+                    )
+                }.frame(width: 0, height: 0)
+                VStack(spacing: 10) {
+                    ForEach(0 ..< 24) {
+                        Text("\($0)")
+                    }
+                }
+            }
+            .coordinateSpace(name: "scrollView")
+            .onPreferenceChange(ScrollOffsetKey.self) { value in
+                let delta = scrollOffset - value
+                scrollOffset = value
+                isScrolling = delta != 0
+                if !isScrolling {
+                    // The scroll view has stopped moving
+                    print("Scrolling stopped")
+                }
+            }
+            .frame(height: 100)
+            Spacer()
+            VStack {
+                Text("Scrolling: \(isScrolling.description)")
+            }
+        }
+    }
+}
+
+struct ScrollOffsetKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
+/*struct UnitTimePicker: View {
+    @State var currentTime: Double = CACurrentMediaTime();
+    @State var lastTime: Double = CACurrentMediaTime();
+    @State var currentPos: CGPoint = CGPoint(x: 0, y: 0)
+    @State var lastPos: CGPoint = CGPoint(x: 0, y: 0)
+    
+    var body: some View {
+        HStack {
+            ScrollViewReader { proxy in
+                BetterScrollView (showsIndicators: false, offsetChanged: {
+                    lastPos = currentPos
+                    currentPos = $0
+                    
+                    lastTime = currentTime;
+                    currentTime = CACurrentMediaTime();
+                }) {
+                    VStack(spacing: 10) {
+                        ForEach(0 ..< 24) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                .frame(height: 100)
+            }
+            Spacer()
+            VStack {
+                Text("Delta: \(currentPos.y - lastPos.y)")
+                Text("Velo: \((currentPos.y - lastPos.y) / (CACurrentMediaTime() - lastTime))")
+                Text("Current: \(currentPos.y)")
+                Text("Last: \(lastPos.y)")
+                Text("Time: \(CACurrentMediaTime())")
+            }
+        }
+    }
+}*/
+
+struct BetterScrollView<Content: View>: View {
+    let axes: Axis.Set
+    let showsIndicators: Bool
+    let offsetChanged: (CGPoint) -> Void
+    let content: Content
+
+    init(
+        axes: Axis.Set = .vertical,
+        showsIndicators: Bool = true,
+        offsetChanged: @escaping (CGPoint) -> Void = { _ in },
+        @ViewBuilder content: () -> Content
+    ) {
+        self.axes = axes
+        self.showsIndicators = showsIndicators
+        self.offsetChanged = offsetChanged
+        self.content = content()
+    }
+    
+    var body: some View {
+        ScrollView(axes, showsIndicators: showsIndicators) {
+            GeometryReader { geometry in
+                Color.clear.preference(
+                    key: ScrollOffsetPreferenceKey.self,
+                    value: geometry.frame(in: .named("scrollView")).origin
+                )
+            }.frame(width: 0, height: 0)
+            content
+        }
+        .coordinateSpace(name: "scrollView")
+        .onPreferenceChange(ScrollOffsetPreferenceKey.self, perform: offsetChanged)
+    }
+}
+
+private struct ScrollOffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGPoint = .zero
+    
+    static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {}
 }
 
 struct ContentView_Previews: PreviewProvider {
