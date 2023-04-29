94af5643aa510525b9f146f50e5c1bcaab8c8c79
Reza Bagheri <rezq.bagheri@gmail.com>
2023-04-28 20:43:16 -0700

    Got the picker view sorted out and basic UI. NGL this amount of work could probably be done in like 30 mins by someone experienced :skull: Now I remember how the rookie programmers feel LMAO

--- WaterProSparkMax/ContentView.swift
+++ WaterProSparkMax/ContentView.swift
@@ -6,153 +6,198 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     @State private var config = Config(
-        table: "", usingPurelyInterval: false, interval: 2000, wateringThreshold: 20, wateringTime: 5000
+        table: "", usingPurelyInterval: false, interval: 0, wateringThreshold: 20, wateringTime: 5000
     )
     
     var body: some View {
+        let tableBinding = Binding<String>(get: {
+            self.config.table
+        }, set: {
+            self.config.table = $0.lowercased()
+        })
+        
         VStack {
             Text("WaterProSparkMax!")
-            TextField("Plant Name", text: $config.table)
+                .bold()
+                .font(.title)
+                .fontWeight(.black)
+            VStack {
+                Text("Interval")
+                IntervalPicker(milliseconds: $config.interval)
+            }
+            VStack {
+                Text("Watering Time")
+                WateringLengthPicker(milliseconds: $config.wateringTime)
+            }
             Toggle("Disable Smart Watering", isOn: $config.usingPurelyInterval)
-            TimePicker()
+                .padding(.top)
+                .padding(.horizontal)
+            TextField("Plant Name", text: tableBinding)
+                .padding()
+                .autocorrectionDisabled(true)
+                .foregroundColor(.white)
+                .background(Color.gray.opacity(0.3))
+                .cornerRadius(15)
+            Button(action: configure) {
+                Text("Configure")
+            }
+            Spacer()
         }
         .padding()
     }
-}
-
-struct TimePicker: View {
-    @State private var days = 0;
-    @State private var hours = 0;
-    @State private var minutes = 0;
-    @State private var seconds = 0;
     
-    var body: some View {
-        UnitTimePicker()
-        .padding()
+    func configure() {
+        
     }
 }
 
-struct UnitTimePicker: View {
-    @State private var scrollOffset: CGFloat = 0
-    @State private var isScrolling = false
-
+struct IntervalPicker: View {
+    @Binding var milliseconds: Int
+    
+    @State var days: String = "0"
+    @State var hours: String = "0"
+    @State var minutes: String = "0"
+    
     var body: some View {
         HStack {
-            ScrollView(showsIndicators: false) {
-                GeometryReader { geometry in
-                    Color.clear.preference(
-                        key: ScrollOffsetKey.self,
-                        value: geometry.frame(in: .named("scrollView")).origin.y
-                    )
-                }.frame(width: 0, height: 0)
-                VStack(spacing: 10) {
-                    ForEach(0 ..< 24) {
-                        Text("\($0)")
+            PickerColumn(timeUnit: "days", range: Array(0...30).map { "\($0)" }, selection: $days)
+                .onChange(of: days) { [days] newStringDays in
+                    let oldDays = Int(days) ?? 0
+                    let newDays = Int(newStringDays) ?? 0
+                    
+                    subtractDays(days: oldDays)
+                    addDays(days: newDays)
+                }
+            PickerColumn(timeUnit: "hours", range: Array(0...24).map { "\($0)" }, selection: $hours)
+                .onChange(of: hours) { [hours] newStringHours in
+                    let oldHours = Int(hours) ?? 0
+                    let newHours = Int(newStringHours) ?? 0
+                    
+                    subtractHours(hours: oldHours)
+                    addHours(hours: newHours)
+                }
+            PickerColumn(timeUnit: "min", range: Array(0...60).map { "\($0)" }, selection: $minutes)
+                .onChange(of: minutes) { [minutes] newStringMinutes in
+                    let oldMinutes = Int(minutes) ?? 0
+                    let newMinutes = Int(newStringMinutes) ?? 0
+                    
+                    subtractMinutes(minutes: oldMinutes)
+                    addMinutes(minutes: newMinutes)
+                }
+        }
+        .frame(height: 200)
+    }
+    
+    func addDays(days: Int) {
+        self.milliseconds += days * Int(8.64e+7)
+    }
+    
+    func subtractDays(days: Int) {
+        self.milliseconds -= days * Int(8.64e+7)
+    }
+    
+    func addHours(hours: Int) {
+        self.milliseconds += hours * Int(3.6e+6)
+    }
+    
+    func subtractHours(hours: Int) {
+        self.milliseconds -= hours * Int(3.6e+6)
+    }
+    
+    func addMinutes(minutes: Int) {
+        self.milliseconds += minutes * 60000
+    }
+    
+    func subtractMinutes(minutes: Int) {
+        self.milliseconds -= minutes * 60000
+    }
+}
+
+struct WateringLengthPicker: View {
+    @Binding var milliseconds: Int
+    
+    @State var seconds: String = "0"
+    
+    var body: some View {
+        HStack {
+            PickerColumn(timeUnit: "sec", range: Array(0...60).map { "\($0)" }, selection: $seconds)
+                .onChange(of: seconds) { [seconds] newStringSeconds in
+                    let oldSeconds = Int(seconds) ?? 0
+                    let newSeconds = Int(newStringSeconds) ?? 0
+                    
+                    subtractSeconds(seconds: oldSeconds)
+                    addSeconds(seconds: newSeconds)
+                }
+        }
+        .frame(height: 200)
+    }
+    
+    func addSeconds(seconds: Int) {
+        self.milliseconds += seconds * 1000
+    }
+    
+    func subtractSeconds(seconds: Int) {
+        self.milliseconds -= seconds * 1000
+    }
+}
+
+struct PickerColumn: View {
+    typealias Label = String
+    typealias Entry = String
+    
+    let timeUnit: Label
+    let range: [Entry]
+    
+    @Binding var selection: Entry
+    
+    var body: some View {
+        HStack() {
+            GeometryReader { geometry in
+                Picker(timeUnit, selection: $selection) {
+                    ForEach(0 ..< range.count, id: \.self) { row in
+                        Text(range[row])
+                            .tag(range[row])
                     }
                 }
+                .frame(width: geometry.size.width, height: geometry.size.height)
+                .pickerStyle(WheelPickerStyle())
+                .clipped()
             }
-            .coordinateSpace(name: "scrollView")
-            .onPreferenceChange(ScrollOffsetKey.self) { value in
-                let delta = scrollOffset - value
-                scrollOffset = value
-                isScrolling = delta != 0
-                if !isScrolling {
-                    // The scroll view has stopped moving
-                    print("Scrolling stopped")
-                }
-            }
-            .frame(height: 100)
-            Spacer()
-            VStack {
-                Text("Scrolling: \(isScrolling.description)")
-            }
+            Text(timeUnit)
+                .bold()
         }
     }
 }
 
-struct ScrollOffsetKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = nextValue()
-    }
-}
+struct MultiPicker: View  {
 
-/*struct UnitTimePicker: View {
-    @State var currentTime: Double = CACurrentMediaTime();
-    @State var lastTime: Double = CACurrentMediaTime();
-    @State var currentPos: CGPoint = CGPoint(x: 0, y: 0)
-    @State var lastPos: CGPoint = CGPoint(x: 0, y: 0)
-    
+    typealias Label = String
+    typealias Entry = String
+
+    let data: [ (Label, [Entry]) ]
+    @Binding var selection: [Entry]
+
     var body: some View {
-        HStack {
-            ScrollViewReader { proxy in
-                BetterScrollView (showsIndicators: false, offsetChanged: {
-                    lastPos = currentPos
-                    currentPos = $0
-                    
-                    lastTime = currentTime;
-                    currentTime = CACurrentMediaTime();
-                }) {
-                    VStack(spacing: 10) {
-                        ForEach(0 ..< 24) {
-                            Text("\($0)")
+        GeometryReader { geometry in
+            HStack {
+                ForEach(0 ..< self.data.count, id: \.self) { column in
+                    Picker(self.data[column].0, selection: self.$selection[column]) {
+                        ForEach(0..<self.data[column].1.count, id: \.self) { row in
+                            Text(verbatim: self.data[column].1[row])
+                                .tag(self.data[column].1[row])
                         }
                     }
+                    .pickerStyle(WheelPickerStyle())
+                    .frame(width: geometry.size.width / CGFloat(self.data.count), height: geometry.size.height)
+                    .clipped()
                 }
-                .frame(height: 100)
-            }
-            Spacer()
-            VStack {
-                Text("Delta: \(currentPos.y - lastPos.y)")
-                Text("Velo: \((currentPos.y - lastPos.y) / (CACurrentMediaTime() - lastTime))")
-                Text("Current: \(currentPos.y)")
-                Text("Last: \(lastPos.y)")
-                Text("Time: \(CACurrentMediaTime())")
             }
         }
     }
-}*/
-
-struct BetterScrollView<Content: View>: View {
-    let axes: Axis.Set
-    let showsIndicators: Bool
-    let offsetChanged: (CGPoint) -> Void
-    let content: Content
-
-    init(
-        axes: Axis.Set = .vertical,
-        showsIndicators: Bool = true,
-        offsetChanged: @escaping (CGPoint) -> Void = { _ in },
-        @ViewBuilder content: () -> Content
-    ) {
-        self.axes = axes
-        self.showsIndicators = showsIndicators
-        self.offsetChanged = offsetChanged
-        self.content = content()
-    }
-    
-    var body: some View {
-        ScrollView(axes, showsIndicators: showsIndicators) {
-            GeometryReader { geometry in
-                Color.clear.preference(
-                    key: ScrollOffsetPreferenceKey.self,
-                    value: geometry.frame(in: .named("scrollView")).origin
-                )
-            }.frame(width: 0, height: 0)
-            content
-        }
-        .coordinateSpace(name: "scrollView")
-        .onPreferenceChange(ScrollOffsetPreferenceKey.self, perform: offsetChanged)
-    }
-}
-
-private struct ScrollOffsetPreferenceKey: PreferenceKey {
-    static var defaultValue: CGPoint = .zero
-    
-    static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {}
 }
 
 struct ContentView_Previews: PreviewProvider {
